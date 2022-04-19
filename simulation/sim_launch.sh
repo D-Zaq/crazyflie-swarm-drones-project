77642f8548a768d3f1c5b81aad5a3ac31d0df8b5
marco <marc.olivier-lemieux@polymtl.ca>
2022-04-19 01:25:13 -0400

    argos folder cleaned

--- simulation/sim_launch.sh
+++ simulation/sim_launch.sh
@@ -1,2 +1,2 @@
-docker build . --tag argos-sim --network host --build-arg UPDATE_CODE=17
+docker build . --tag argos-sim --network host --build-arg UPDATE_CODE=18
 sudo x11docker --hostdisplay --hostnet --user=RETAIN -- --privileged -v -- argos-sim
